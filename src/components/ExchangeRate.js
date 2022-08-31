94964b0651b60154cdc213446ad254ba5d841173
RaelynCS <raelyn.cheungsutton@slalom.com>
2022-08-31 13:51:59 -0400

    replaced mapStateToProps with useSelector hook

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { ratesUpdated } from "../store/actions/RateActions";
 import {
   getCurrencyCode,
@@ -11,55 +11,51 @@ import { CurrencyCodePickerContainer } from "./CurrencyCodePicker";
 import { getExchangeRates } from "../api";
 import { AmountFieldContainer } from "./AmountField";
 
-export function ExchangeRate({ currencyCode, updateRates, supportedCurrencies }) {
+export function ExchangeRate({ updateRates }) {
+  const supportedCurrencies = useSelector(getSupportedCurrencies);
+  const currencyCode = useSelector(getCurrencyCode);
+
+  // can also be written: const currencyCode = useSelector((state) => getCurrencyCode(state));
+  // or const currencyCode = useSelector((state) => state.rates.supportedCurrencies)
 
   useEffect(() => {
-      getLatestExchangeRates();
+    getLatestExchangeRates();
   }, [currencyCode]);
 
-  
   function getLatestExchangeRates() {
     getExchangeRates(currencyCode, supportedCurrencies).then((rates) => {
       updateRates(rates);
     });
   }
-    return (
-      <>
-        <section>
-          <h1 className="ExchangeRate-header">
-            Exchange Rates <CurrencyCodePickerContainer />
-          </h1>
-        </section>
-        <section>
-          <AmountFieldContainer />
-        </section>
-        <section>
-          <RateTableContainer />
-        </section>
-      </>
-    );
+  return (
+    <>
+      <section>
+        <h1 className="ExchangeRate-header">
+          Exchange Rates <CurrencyCodePickerContainer />
+        </h1>
+      </section>
+      <section>
+        <AmountFieldContainer />
+      </section>
+      <section>
+        <RateTableContainer />
+      </section>
+    </>
+  );
 }
 
 // props types
 ExchangeRate.propTypes = {
   updateCurrencyCode: PropTypes.func,
-  currencyCode: PropTypes.string,
-  supportedCurrencies: PropTypes.arrayOf(PropTypes.string),
 };
 
 // redux stuff
-function mapStateToProps(state) {
-  return {
-    supportedCurrencies: getSupportedCurrencies(state),
-    currencyCode: getCurrencyCode(state),
-  };
-}
 function mapDispatchToProps(dispatch) {
   return {
     updateRates: (rates) => dispatch(ratesUpdated(rates)),
   };
 }
 export const ExchangeRateContainer = connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(ExchangeRate);
