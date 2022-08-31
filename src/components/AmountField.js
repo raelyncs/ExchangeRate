f6630a6d8ed474e8d7f950e72f721026f0143830
RaelynCS <raelyn.cheungsutton@slalom.com>
2022-08-31 13:31:52 -0400

    created a debouned event handler w the useMemo hook

--- src/components/AmountField.js
+++ src/components/AmountField.js
@@ -1,29 +1,28 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getAmount } from "../store/reducers/RateReducer";
 import { amountChanged } from "../store/actions/RateActions";
 import { debounce } from "lodash";
 
-export class AmountField extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { amount: props.amount };
-    this.onChange = this.onChange.bind(this);
-    this.changeAmount = debounce(this.props.changeAmount, 500);
-  }
-  onChange(e) {
+export function AmountField({ amount, changeAmount }) {
+  const [displayAmount, setDisplayAmount] = useState(amount);
+  // useMemo
+  const onAmountChange = useMemo(
+    () => debounce(changeAmount, 500),
+    [changeAmount]
+  );
+
+  function onChange(e) {
     let newAmount = e.target.value;
-    this.setState({ amount: newAmount });
-    this.changeAmount(newAmount);
+    setDisplayAmount(newAmount);
+    onAmountChange(newAmount);
   }
-  render() {
-    return (
-      <form className="ExchangeRate-form">
-        <input type="text" value={this.state.amount} onChange={this.onChange} />
-      </form>
-    );
-  }
+  return (
+    <form className="ExchangeRate-form">
+      <input type="text" value={displayAmount} onChange={onChange} />
+    </form>
+  );
 }
 
 // prop types
